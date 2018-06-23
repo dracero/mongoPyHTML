f74cc129ce89f7678288cdef56691e1b42a7cf26
root <diego.racero@gmail.com>
2018-06-23 19:27:51 -0300

    index

--- blogPostDAO.py
+++ blogPostDAO.py
@@ -23,6 +23,7 @@ import re
 import datetime
 
 
+
 # The Blog Post Data Access Object handles interactions with the Posts collection
 class BlogPostDAO:
 
@@ -53,9 +54,7 @@ class BlogPostDAO:
 
         # now insert the post
         try:
-            # XXX HW 3.2 Work Here to insert the post
             self.posts.insert_one(post)
-            
             print "Inserting the post"
         except:
             print "Error inserting post"
@@ -63,21 +62,10 @@ class BlogPostDAO:
 
         return permalink
 
-    # returns an array of num_posts posts, reverse ordered by date.
+    # returns an array of num_posts posts, reverse ordered
     def get_posts(self, num_posts):
 
-        cursor = iter(())  # Using an empty itable for a placeholder so blog compiles before you make your changes
-
-        # XXX HW 3.2 Work here to get the posts
-        query = { }
-        
-        try:
-            cursor = self.posts.find(query).sort('date', -1).limit(num_posts)
-            print "Getting the post by its permalink"
-        except:
-            print "Error getting post"
-
-
+        cursor = self.posts.find().sort('date', direction=-1).limit(num_posts)
         l = []
 
         for post in cursor:
@@ -95,20 +83,38 @@ class BlogPostDAO:
 
         return l
 
+    # returns an array of num_posts posts, reverse ordered, filtered by tag
+    def get_posts_by_tag(self, tag, num_posts):
+
+        cursor = self.posts.find({'tags':tag}).sort('date', direction=-1).limit(num_posts)
+        l = []
+
+        for post in cursor:
+            post['date'] = post['date'].strftime("%A, %B %d %Y at %I:%M%p")     # fix up date
+            if 'tags' not in post:
+                post['tags'] = []           # fill it in if its not there already
+            if 'comments' not in post:
+                post['comments'] = []
+
+            l.append({'title': post['title'], 'body': post['body'], 'post_date': post['date'],
+                      'permalink': post['permalink'],
+                      'tags': post['tags'],
+                      'author': post['author'],
+                      'comments': post['comments']})
+
+        return l
+
     # find a post corresponding to a particular permalink
     def get_post_by_permalink(self, permalink):
 
-        post = None
-        # XXX 3.2 Work here to retrieve the specified post
-        query = { 'permalink': permalink }
+        post = self.posts.find_one({'permalink': permalink})
 
-        try:
-            post = self.posts.find_one(query)
-            print "Getting the post by its permalink"
-        except:
-            print "Error getting post"
-            
         if post is not None:
+            # fix up likes values. set to zero if data is not present
+            for comment in post['comments']:
+                if 'num_likes' not in comment:
+                    comment['num_likes'] = 0
+
             # fix up date
             post['date'] = post['date'].strftime("%A, %B %d %Y at %I:%M%p")
 
@@ -123,21 +129,21 @@ class BlogPostDAO:
             comment['email'] = email
 
         try:
-            # XXX HW 3.3 Work here to add the comment to the designated post. When done, modify the line below to return the number of documents updated by your modification, rather than just -1.
-            query = { 'permalink': permalink }
-            post_find = self.posts.find_one(query)          # finding post in which comment is be added 
-            
-            comment_list = post_find['comments']             # storing comments in a list
-            comment_list.append(comment)                    # appending new comment in the list
-            
-            update = { '$set': {'comments': comment_list} }  
-            self.posts.update_one(query, update, upsert=True)   # updating the document by the new list   
-            
-            post_find = self.posts.find_one(query)          # finding post in which comment is be added 
+            update_result = self.posts.update_one({'permalink': permalink}, {'$push': {'comments': comment}})
+                                               
 
-            return post_find  # Change this to return the number of documents updated by the code for HW 3.3
+            return update_result.matched_count
 
         except:
             print "Could not update the collection, error"
             print "Unexpected error:", sys.exc_info()[0]
-        return 0
+            return 0
+
+
+
+
+
+
+
+
+

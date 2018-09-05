728a3ee944bd80a31f46defaff7d4cd145ccf68b
cristian3629 <cristian3629@gmail.com>
2018-09-05 18:55:05 -0300

    re sultado como arreglo

--- atlas.py
+++ atlas.py
@@ -18,8 +18,18 @@ class Atlas:
         #pprint.pprint(elements.find_one({"Email": email}))
         elemento = elements.find_one({"Email": email})
         Parcial1 = elemento["Parcial1"]
-        print Parcial1
-        return elemento
+        Email = elemento["Email"]
+        Parcial1 = elemento["Parcial1"]
+        Parcial2  = elemento["Parcial2"]
+        Parcial3  = elemento["Parcial3"]
+        RecParcial1 = elemento["RecParcial1"]
+        RecParcial2 = elemento["RecParcial2"]
+        RecParcial3 = elemento["RecParcial3"]
+        Rec2Parcial1 = elemento["2RecParcial1"]
+        Rec2Parcial2 = elemento["2RecParcial2"]
+        resultado = [Email,Parcial1,Parcial2,Parcial3,RecParcial1,RecParcial2,RecParcial3,Rec2Parcial1,Rec2Parcial2]
+        #print Parcial1
+        return resultado[0]
 
 
     def importar(self, archivo):
